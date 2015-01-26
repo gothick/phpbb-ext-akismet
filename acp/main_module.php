c8c959d72b8a549bfb3ae03728333b3bcf5aeabf
Matt Gibson <gothick@gothick.org.uk>
2015-01-26 13:42:17 +0000

    Add basic option for a nominated user to receive emails/be responsible for log entries, and log as/send emails to that user.

--- acp/main_module.php
+++ acp/main_module.php
@@ -1,47 +1,88 @@
 <?php
 /**
-*
-* @package phpBB Extension - Gothick Akismet
-* @copyright (c) 2015 Matt Gibson Creative Ltd.
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
-
+ *
+ * @package phpBB Extension - Gothick Akismet
+ * @copyright (c) 2015 Matt Gibson Creative Ltd.
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 namespace gothick\akismet\acp;
 
 class main_module
 {
+
 	var $u_action;
 
-	function main($id, $mode)
+	function main ($id, $mode)
 	{
 		global $db, $user, $auth, $template, $cache, $request, $phpbb_log;
 		global $config, $phpbb_root_path, $phpbb_admin_path, $phpEx;
-
+		
 		$user->add_lang('acp/common');
 		$this->tpl_name = 'akismet_body';
 		$this->page_title = $user->lang('GOTHICK_AKISMET_TITLE');
 		add_form_key('gothick/akismet');
-
-		if ($request->is_set_post('submit'))
-		{
-			if (!check_form_key('gothick/akismet'))
-			{
+		
+		if ($request->is_set_post('submit')) {
+			if (! check_form_key('gothick/akismet')) {
 				trigger_error('FORM_INVALID');
 			}
-
-			$config->set('gothick_akismet_api_key', $request->variable('gothick_akismet_api_key', ''));
-			$config->set('gothick_akismet_url', $request->variable('gothick_akismet_url', ''));
-
-			$phpbb_log->add('admin', $user->data['user_id'], $user->ip, 'ACP_AKISMET_SETTINGS_CHANGED');
 			
-			trigger_error($user->lang('GOTHICK_AKISMET_SETTING_SAVED') . adm_back_link($this->u_action));
+			$config->set('gothick_akismet_api_key', 
+					$request->variable('gothick_akismet_api_key', ''));
+			$config->set('gothick_akismet_url', 
+					$request->variable('gothick_akismet_url', ''));
+			
+			$username = utf8_normalize_nfc(
+					request_var('gothick_akismet_username', '', true));
+			$sql = 'SELECT user_id
+				FROM ' . USERS_TABLE .
+					 "
+				WHERE username_clean = '" .
+					 $db->sql_escape(utf8_clean_string($username)) . "'";
+			$result = $db->sql_query($sql);
+			$user_id = (int) $db->sql_fetchfield('user_id');
+			$db->sql_freeresult($result);
+			
+			if (! $user_id) {
+				trigger_error(
+						$user->lang['NO_USER'] . adm_back_link($this->u_action), 
+						E_USER_WARNING);
+			} else {
+				$config->set('gothick_akismet_user_id', $user_id);
+			}
+			
+			$phpbb_log->add('admin', $user->data['user_id'], $user->ip, 
+					'ACP_AKISMET_SETTINGS_CHANGED');
+			
+			trigger_error(
+					$user->lang('GOTHICK_AKISMET_SETTING_SAVED') .
+							 adm_back_link($this->u_action));
 		}
-
-		$template->assign_vars(array(
-			'U_ACTION'					=> $this->u_action,
-			'GOTHICK_AKISMET_API_KEY'	=> $config['gothick_akismet_api_key'],
-			'GOTHICK_AKISMET_URL'		=> $config['gothick_akismet_url'],
-		));
+		
+		$username = '';
+		if (isset($config['gothick_akismet_user_id'])) {
+			$user_id = filter_var($config['gothick_akismet_user_id'], 
+					FILTER_VALIDATE_INT);
+			if ($user_id !== false) {
+				$sql = 'SELECT u.username FROM ' . USERS_TABLE .
+						 ' u WHERE u.user_id = ' . $user_id;
+				$result = $db->sql_query_limit($sql, 1);
+				$user_row = $db->sql_fetchrow($result);
+				$db->sql_freeresult($result);
+				
+				if ($user_row) {
+					$username = $user_row['username'];
+				}
+			}
+		}
+		
+		$template->assign_vars(
+				array(
+						'U_ACTION' => $this->u_action,
+						'GOTHICK_AKISMET_API_KEY' => $config['gothick_akismet_api_key'],
+						'GOTHICK_AKISMET_URL' => $config['gothick_akismet_url'],
+						'GOTHICK_AKISMET_USERNAME' => $username
+				));
 	}
 }
--- event/main_listener.php
+++ event/main_listener.php
@@ -50,12 +50,24 @@ class main_listener implements EventSubscriberInterface
 
 	/* @var \phpbb\request\request */
 	protected $request;
-	
+
 	/* @var \phpbb\config\config */
 	protected $config;
-	
-	// vendor Akismet client library
+
+	/* @var \phpbb\log\log */
+	protected $log;
+
+	/* @var \phpbb\user_loader */
+	protected $user_loader;
+
+	/* @var \TijsVerkoyen\Akismet */
 	protected $akismet;
+ // Third-party client library
+	
+	/* @var \messenger */
+	protected $messenger;
+
+	protected $akismet_user_data;	
 
 	/**
 	 * Constructor
@@ -64,18 +76,22 @@ class main_listener implements EventSubscriberInterface
 	 *        	object
 	 * @param \phpbb\template $template        	
 	 * @param \phpbb\user $user        	
-	 * @param \phpbb\request\request $request
+	 * @param \phpbb\request\request $request        	
 	 * @param \phpbb\config\config $request        	
+	 * @param \phpbb\log\log $log        	
+	 * @param \phpbb\user_loader $user_loader        	
 	 */
 	public function __construct (\phpbb\controller\helper $helper, 
 			\phpbb\template\template $template, \phpbb\user $user, 
-			\phpbb\request\request $request,
-			\phpbb\config\config $config)
+			\phpbb\request\request $request, \phpbb\config\config $config, 
+			\phpbb\log\log $log, \phpbb\user_loader $user_loader)
 	{
 		$this->helper = $helper;
 		$this->template = $template;
 		$this->user = $user;
 		$this->config = $config;
+		$this->log = $log;
+		$this->user_loader = $user_loader;
 		
 		// TODO: Remove $request when we don't need it to allow access to
 		// $_SERVER any more.
@@ -88,6 +104,31 @@ class main_listener implements EventSubscriberInterface
 				 isset($config['gothick_akismet_url'])) {
 			$this->akismet = new Akismet($config['gothick_akismet_api_key'], 
 					$config['gothick_akismet_url']);
+			
+			// We log, send mail, etc. as our Akismet user, and we want the 
+			// results to be in their language, not the language of the user
+			// who's posting.  
+			if (isset($config['gothick_akismet_user_id'])) {
+				$akismet_user_id = filter_var($config['gothick_akismet_user_id'], FILTER_VALIDATE_INT);
+				if ($akismet_user_id !== false) {
+					// We load the Akismet user's common language file, plus this extension's
+					// langauge file. That way we can send emails/log messages in the target
+					// user's language, not the current user's language. 
+					$this->akismet_user_data = $user_loader->get_user($akismet_user_id, true);
+				}
+			} else {
+				// TODO: Should this be an error?
+			}
+			
+			// For email sending
+			if ($this->config['email_enable']) {
+				if (! class_exists('messenger')) {
+					global $phpbb_root_path, $phpEx;
+					include ($phpbb_root_path . 'includes/functions_messenger.' .
+							 $phpEx);
+					$this->messenger = new \messenger(false);
+				}
+			}
 		}
 	}
 
@@ -113,12 +154,42 @@ class main_listener implements EventSubscriberInterface
 				));
 	}
 
+	protected function send_mail($post_data)
+	{
+		if (isset($this->messenger)) 
+		{
+			// If we have a nominated Akismet user, we send them an email to let them
+			// know the message has been marked as spam:
+			if (isset($this->akismet_user_data))
+			{
+				$this->messenger->template('@gothick_akismet/message_marked_as_spam', $this->akismet_user_data['user_lang']);
+				$this->messenger->to($this->akismet_user_data['user_email'], $this->akismet_user_data['username']);
+				$this->messenger->im($this->akismet_user_data['user_jabber'], $this->akismet_user_data['username']);
+				$this->messenger->assign_vars(array(
+					'TOPIC_TITLE'	=> $post_data['topic_title'],
+					'POSTING_USER_USERNAME'	=> $this->user->data['username'],
+					'POSTING_USER_URL' => generate_board_url().'/memberlist.php?mode=viewprofile&u=' . $this->user->data['user_id'], 
+					'POST_TEXT' => $post_data['message']  
+				));
+				$this->messenger->subject('Forum spam detected from user ' . $this->user->data['username_clean']);
+				$this->messenger->headers('X-AntiAbuse: User IP - ' . $this->user->ip);
+				
+				$this->messenger->send($this->akismet_user_data['user_notify_type']);
+				$this->messenger->save_queue();
+				
+			}
+		} 
+	}
+
 	public function check_submitted_post ($event)
 	{
 		// TODO: Some kind of (quiet) error logging if the Akismet object hasn't
-		// been
-		// created (might happen if API key isn't set.) Admin log?
-		if (isset($this->akismet)) {
+		// been created (might happen if API key isn't set.) Admin log?
+		// Use
+		// https://github.com/ForumHulp/errorpages/blob/master/event/listener.php
+		// for logging example
+		if (isset($this->akismet)) 
+		{
 			
 			$data = $event['data'];
 			
@@ -137,7 +208,7 @@ class main_listener implements EventSubscriberInterface
 			// https://www.phpbb.com/community/viewtopic.php?f=461&t=2267121&p=13760226&hilit=username#p13760226
 			// $this->profilefields->grab_profile_fields_data($user_id)
 			
-			// TODO:
+			// TODO: URL and permalink parameters
 			$url = '';
 			$permalink = '';
 			
@@ -148,20 +219,18 @@ class main_listener implements EventSubscriberInterface
 			$this->request->enable_super_globals();
 			
 			$is_spam = false;
-			try 
-			{
+			try {
 				// 'forum-post' recommended for type:
 				// http://blog.akismet.com/2012/06/19/pro-tip-tell-us-your-comment_type/
-				$is_spam = $this->akismet->isSpam($content, $author, $email, $url, 
-						$permalink, 'forum-post');
-			}
-			//TODO: The Akismet class actually throws its own TijsVerkoyen\Akismet\Exception. Should
-			// we be checking for that/ 
-			catch (\Exception $e) 
-			{
-				// If Akismet's down, or there's some other problem like that, we'll
-				// give the post the benefit of the doubt.
-				// TODO: Error logging. 
+				$is_spam = $this->akismet->isSpam($content, $author, $email, 
+						$url, $permalink, 'forum-post');
+			}			// TODO: The Akismet class actually throws its own
+			// TijsVerkoyen\Akismet\Exception. Should
+			// we be checking for that/
+			catch (\Exception $e) {
+				// If Akismet's down, or there's some other problem like that,
+				// we'll give the post the benefit of the doubt.
+				// TODO: Log warning
 			}
 			
 			$this->request->disable_super_globals();
@@ -171,6 +240,28 @@ class main_listener implements EventSubscriberInterface
 				// and mark it as unapproved.
 				$data['force_approved_state'] = ITEM_UNAPPROVED;
 				$event['data'] = $data;
+				
+				// Note our action in the moderation log
+				$log_message = $event['mode'] == 'reply' ? 'LOG_POST_DISAPPROVED' : 'LOG_TOPIC_DISAPPROVED';
+				
+				$akismet_user_id = isset($this->akismet_user_data) ? $this->akismet_user_data['user_id'] : $this->user->data['user_id'];
+				$akismet_username = isset($this->akismet_user_data) ? $this->akismet_user_data['username'] : $this->user->data['username'];
+				
+				$this->log->add('mod', $this->akismet_user_data['user_id'],
+						$this->user->data['session_ip'], $log_message, false,  // Logger will provide the time
+						array(
+								$data['topic_title'],
+								// TODO: We should log in the language of the nominated Akismet user. This has
+								// been a nightmare to figure out, though, and got very messy, so we're just 
+								// going to log stuff in the language of the posting user for now. This 
+								// should be okay for most boards, as it's only in multilingual boards 
+								// where the user's language would be different from a board admin's 
+								// language. Revisit when (if?) phpBB makes this easier. 
+								$this->user->lang('AKISMET_DISAPPROVED'), 
+								$this->user->data['username']
+						));
+				
+				$this->send_mail($data);
 			}
 		}
 	}
