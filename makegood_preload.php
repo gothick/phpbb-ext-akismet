b8dedfb70c6c867d555240b6ed039358ea349f1b
Matt Gibson <gothick@gothick.org.uk>
2015-01-28 16:05:45 +0000

    Add configuration file for MakeGood local continuous testing.

---
+++ makegood_preload.php
@@ -0,0 +1,5 @@
+<?php
+	// Need autoload so we can actually use PHPUnit.
+	require ( __DIR__ . '/../../../vendor/autoload.php');
+	// phpBB tests are set up so that we *must* be in the phpbb directory
+	chdir( __DIR__ . '/./../../../..');
